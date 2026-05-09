c4d92e323e004e771b328862f3c92ecd4a7011b7
Heewoong Kim <khw7488khw@gmail.com>
2026-05-09 16:16:16 +0900

    w01_fixed

--- backend/mnt/user-data/outputs/backend/controllers/professional.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const professionalController = require('../controllers/professional');
-
-router.get('/', professionalController.getData);
-
-module.exports = router;
